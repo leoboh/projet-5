45995e13f6b37d0b4c2dd641447173675702c542
gurso <account+gitlab@urso.fr>
2022-02-03 18:22:13 +0100

    starter pack p5

---
+++ front/js/product.js
@@ -0,0 +1,5 @@
+console.log("product")
+
+// aller chercher les infos du bon canape
+/// recuperer l'id dans l'url
+/// chercher donnes sur le serveur
---
+++ front/js/script.js
@@ -0,0 +1,9 @@
+console.log("coucou")
+
+// recupere les produits sur le serveur
+/// trouver l'url =>
+/// faire la requete http
+
+// les afficher
+// transformer les donnees en "html"
+// les ajouter au DOM
