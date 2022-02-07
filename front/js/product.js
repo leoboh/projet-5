8e4cc9f4b0f9ca6093c8f1361a857c7dc17828d6
leo blanche <leo.firon@live.fr>
2022-02-07 19:06:52 +0100

    m

---
+++ front/js/product.js
@@ -0,0 +1,3 @@
+// aller chercher les infos du bon canape
+/// recuperer l'id dans l'url
+/// chercher donnes sur le serveur
---
+++ front/js/script.js
@@ -0,0 +1,7 @@
+// recupere les produits sur le serveur
+/// trouver l'url =>
+/// faire la requete http
+
+// les afficher
+// transformer les donnees en "html"
+// les ajouter au DOM
