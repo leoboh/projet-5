964d78a70f29964056a62cdb7c3f62d97f9dba08
leo blanche <leo.firon@live.fr>
2022-02-10 18:07:19 +0100

    m

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-console.log("product")
-
->>>>>>> 45995e13f6b37d0b4c2dd641447173675702c542
 // aller chercher les infos du bon canape
 /// recuperer l'id dans l'url
 /// chercher donnes sur le serveur
--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,38 @@
-// recupere les produits sur le serveur
-/// trouver l'url =>
-/// faire la requete http
+// appel de l'api qui va stock les données dans la variable "resultat"
+/*fetch("http://localhost:3000/api/products").then((resultat) => {
+  // si les données ont bien été récuperés alors ...
+  if (resultat.ok) {
+    // la variable "data" va heriter de de la variable "resultat"
+    resultat.json().then((data) => {
+      // la variable "produits" va heriter de "data"
+      produits = data.resultat;
+    });
+  } else {
+    alert("il y a une erreur");
+  }
+});
 
+var produits = {};
+console.log(produits);
 // les afficher
+
 // transformer les donnees en "html"
 // les ajouter au DOM
+*/
+
+fetch("http://localhost:3000/api/products")
+  .then((resp) => resp.json()) // pourquoi then accepte pas la l'attribut function "namefunction"
+  .then(function recupData(data) {
+    // on stock automatiquement les donnes dans resp ? qu'on a traduit et ensuite c'est automatiquement mis dans data ?
+    let produits = data.resultat;
+  });
+
+function produit(couleur, id, nom, prix, img, description, alt) {
+  (this.couleur = couleur),
+    (this.id = id),
+    (this.nom = nom),
+    (this.prix = prix),
+    (this.img = img),
+    (this.description = description),
+    (this.alt = alt);
+}
