38df97861baf58644861c9fd33419cdf6523dc49
leo blanche <leo.firon@live.fr>
2022-02-24 17:50:10 +0100

    m

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,39 @@
-// aller chercher les infos du bon canape
-/// recuperer l'id dans l'url
+// recuperer l'url
+let recupUrl = window.location.search;
+
+// recuperer l'id dans l'url (on recuperer id (?id=) et non l'id du produit(098765576879))
+let recupId = new URLSearchParams(recupUrl);
+let idUrl = recupId.get("id");
+
 /// chercher donnes sur le serveur
+fetch("http://localhost:3000/api/products")
+  .then((resp) => resp.json())
+  .then(function (produits) {
+    let produitUrl = produits.find((produit) => produit._id === idUrl); // Chercher dans les produits, le produit pour lequel l'id est =
+
+    let nomProduit = document.getElementById("title");
+    let prixProduit = document.getElementById("price");
+    let descriptionProduit = document.getElementById("description");
+
+    // creation et insertion de l'img dans le dom
+    let imgProduit = document.createElement("img");
+    let parentImgProduit = document.querySelector(".item__img");
+    parentImgProduit.appendChild(imgProduit);
+    imgProduit.src = produitUrl.imageUrl;
+    //
+
+    // creation et insertion des couleur dans le dom
+    let couleurProduit = document.createElement("option");
+    let parentCouleurProduit = document.getElementById("colors");
+    parentCouleurProduit.appendChild(couleurProduit);
+    couleurProduit.textContent = produitUrl.colors;
+    //
+    console.log(produitUrl.colors);
+
+    nomProduit.textContent = produitUrl.name;
+    prixProduit.textContent = produitUrl.price;
+    descriptionProduit.textContent = produitUrl.description;
+  });
+
+//recupere les données serveur
+//recuperer les données de l'id correspondant a idUrl
--- front/js/script.js
+++ front/js/script.js
@@ -1,54 +1,26 @@
-// appel de l'api qui va stock les données dans la variable "resultat"
-/*fetch("http://localhost:3000/api/products").then((resultat) => {
-  // si les données ont bien été récuperés alors ...
-  if (resultat.ok) {
-    // la variable "data" va heriter de de la variable "resultat"
-    resultat.json().then((data) => {
-      // la variable "produits" va heriter de "data"
-      produits = data.resultat;
-    });
-  } else {
-    alert("il y a une erreur");
-  }
-});
-
-var produits = {};
-console.log(produits);
-// les afficher
-
-// transformer les donnees en "html"
-// les ajouter au DOM
-*/
-
 fetch("http://localhost:3000/api/products")
   .then((resp) => resp.json())
   .then(function (produits) {
     for (let produit of produits) {
-      // pour chaque produit dans produits
+      // pour chaque produit dans produits je crée une balise dans le DOM
       let a = document.createElement("a"); // je crée une balise "a"
-      let article = document.createElement("article");
-      let img = document.createElement("img");
-      let titre = document.createElement("h3");
-      let p = document.createElement("p");
-      let section = document.getElementById("items");
+      let article = document.createElement("article"); //...
+      let img = document.createElement("img"); //...
+      let titre = document.createElement("h3"); //...
+      let p = document.createElement("p"); //...
+      let section = document.getElementById("items"); //...
 
-      a.href = "./product.html?id=${produit._id}";
-      img.src = produit.imageUrl;
-      titre.textContent = produit.name;
-      p.textContent = produit.description;
+      // pour chaque balises créé j'assigne leurs place dans le DOM en fonction de la balise parent
+      section.appendChild(a); // La variable a sera l'enfant de la variable section
+      a.appendChild(article); //...
+      article.appendChild(img); //...
+      article.appendChild(titre); //...
+      article.appendChild(p); //...
 
-      section.appendChild(a);
-      a.appendChild(article);
-      article.appendChild(img);
-      article.appendChild(titre);
-      article.appendChild(p);
-
-      /*let id = produit._id;
-      let couleur = produit.color;
-      let nom = produit.name;
-      let prix = produit.price;
-      let image = produit.imageUrl;
-      let desc = produit.description;
-      let alt = produit.altTxt;*/
+      // on inserer les valeurs des produits dans les variables
+      a.href = "./product.html?id=" + produit._id; // insertion de l'id dans la balise a
+      img.src = produit.imageUrl; //...
+      titre.textContent = produit.name; //...
+      p.textContent = produit.description; //...
     }
   });
